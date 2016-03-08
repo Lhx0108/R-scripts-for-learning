2333fefdb7b3311398cca9c4f897bde6f175ee2c
Lhx0108 <haoxinli0108@gmail.com>
2016-03-07 20:46:33 -0500

    final version

--- get_unknown_from_grape.R
+++ get_unknown_from_grape.R
@@ -31,23 +31,26 @@ get.unknowns.from.files <- function(list.of.files){
 #will add one row and 0-n columns
 add.to.matrix <- function(file1, unknown.matrix){
   unknown.results <- fromJSON(file = file1)
-  for(i in 1:length(unknown.results[[1]]$fragments)){
+  for(i in 1:length(unknown.results[[1]]$fragments))
     current.query <- unknown.results[[1]]$fragments[[i]]
-    for(j in 1:length(current.query))
-      subquery <- current.query[[j]]
-    for(h in 1: length(subquery))
-      sub.subquery <- subquery[[h]]
-    if(is.null(sub.subquery) & is.null(subquery$smiles)){
-      subquery$smiles <- NA
-    } else if(subquery$type == "UNKNOWN_OTHER"){
-      unknown.matrix <- rbind(unknown.matrix, subquery$smiles)
-    } else if (is.null(sub.subquery$smiles)){
-      sub.subquery$smiles <- NA}
-    else if(sub.subquery$type == "UNKNOWN_OTHER"){
-      unknown.matrix <- rbind(unknown.matrix, sub.subquery$smiles)
+  for(j in 1:length(current.query))
+    subquery <- current.query[[j]]
+  if(is.character(subquery$smiles) & !is.null(subquery$smiles) & subquery$type == "UNKNOWN_OTHER"){
+    unknown.matrix <- rbind(unknown.matrix, subquery$smiles)
+  } 
+  else{
+    for(h in 1: length(subquery)) 
+    sub.subquery <- subquery[[h]]
+    if(is.list(sub.subquery)){
+      unknown.matrix <- get.unknown.step.2(sub.subquery, unknown.matrix)
     }
-    
   }
   return(unknown.matrix)
 }
-
+    get.unknown.step.2 <- function(sub.subquery, unknown.matrix){
+    if(is.character(sub.subquery$smiles) & !is.null(sub.subquery$smiles) & sub.subquery$type == "UNKNOWN_OTHER"){
+    unknown.matrix <- rbind(unknown.matrix, sub.subquery$smiles)}
+    }
+    
+    
+    
